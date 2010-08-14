07dec4de4f84f5d22318098ec48519027773cf96
chengdh <cheng.donghui@gmail.com>
2010-08-14 09:28:31 +0800

    change generator

--- generators/douban/douban_generator.rb
+++ generators/douban/douban_generator.rb
@@ -1,20 +1,49 @@
-require 'rails/generators'
-
-class DoubanGenerator < Rails::Generators::Base
-  def install_douban
-    copy_file('douban_controller.rb', 'app/controllers/douban_controller.rb')
-    copy_file("douban_helper.rb", 'app/helpers/douban_helper.rb')
-    copy_file("douban.yml", 'config/douban.yml')
-    
-    route %(# Routes for Douban OAuth
-        map.with_options :controller => 'douban' do |douban|
-          douban.new        '',  :action  => 'new'
-          douban.callback   '',  :action  => 'callback',
-          douban.logout     '',  :action  => 'logout'
-        end)
+class DoubanGenerator < Rails::Generator::Base
+  def manifest  
+    record do |m|  
+      m.file 'douban_controller.rb', 'app/controllers/douban_controller.rb'
+      m.file "douban_helper.rb", 'app/helpers/douban_helper.rb'
+      m.file "douban.yml", 'config/douban.yml'
+      m.routh :douban_login,:controller => :douban,:action => :new
+      m.routh :douban_callback,:controller => :douban,:action => :callback
+      m.routh :douban_logout,:controller => :douban,:action => :destroy
+    end  
   end
-  
-  def self.source_root
-    File.join(File.dirname(__FILE__), 'templates')
+end
+#为rails genetator添加处理代码
+module Rails
+  module Generator
+    module Commands
+      class Base
+        def route_code(route_options)
+          "map.#{route_options[:name]} '#{route_options[:name]}', :controller => '#{route_options[:controller]}', :action => '#{route_options[:action]}'"
+        end
+      end
+
+      # Here's a readable version of the long string used above in route_code;
+      # but it should be kept on one line to avoid inserting extra whitespace
+      # into routes.rb when the generator is run:
+      # "map.#{route_options[:name]} '#{route_options[:name]}',
+      #     :controller => '#{route_options[:controller]}',
+      #     :action => '#{route_options[:action]}'"
+
+      class Create
+        def route(route_options)
+          sentinel = 'ActionController::Routing::Routes.draw do |map|'
+          logger.route route_code(route_options)
+          gsub_file 'config/routes.rb', /(#{Regexp.escape(sentinel)})/mi do |m|
+            "#{m}\n  #{route_code(route_options)}\n"
+          end
+        end
+      end
+
+      class Destroy
+        def route(route_options)
+          logger.remove_route route_code(route_options)
+          to_remove = "\n  #{route_code(route_options)}"
+          gsub_file 'config/routes.rb', /(#{to_remove})/mi, ''
+        end
+      end
+    end
   end
 end
